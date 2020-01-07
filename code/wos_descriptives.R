37b5ffd772daf43aa19da5019501eabdf6162b58
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-01-07 18:00:21 -0500

    cleaning code, formatting figures.

--- code/wos_descriptives.R
+++ code/wos_descriptives.R
@@ -13,8 +13,7 @@ library(ggplot2)
 library(forcats)
 
 # load data
-data <- read.csv('/Users/brandonsepulvado/Documents/synbio/data/web_of_science/data_subset.csv',
-                 header = TRUE)
+data <- read.csv(here('data', 'web_of_science', 'data_subset.csv'), header = TRUE)
 
 # descriptives =================================================================
 
@@ -26,9 +25,13 @@ data %>%
   ggplot(aes(x = pub_year, y = n)) +
   geom_point() +
   geom_line() +
+  geom_vline(xintercept = 1990, color = 'orange2') +
   theme_minimal() +
   labs(x = 'Publication Year',
-       y = 'Number of Publications')
+       y = 'Number of Publications',
+       title = 'Number of Publications Per Year, 1900-2019',
+       subtitle = 'Source: Web of Science',
+       caption = 'Note: Vertical line denotes 1990; years without publications have no point.')
 
 # top keywords
 data %>% 
@@ -42,7 +45,10 @@ data %>%
   coord_flip() +
   theme_minimal() +
   labs(y = 'Number of Occurrences',
-       x = NULL)
+       x = NULL,
+       title = 'Top 20 Author Keywords, 1900-2019',
+       subtitle = 'Source: Web of Science'
+       )
 
 # open access versions
 data %>% 
@@ -63,8 +69,10 @@ data %>%
   geom_col(show.legend = FALSE) +
   coord_flip() +
   theme_minimal() +
-  labs(y = 'Number of Occurrences',
-       x = NULL)
+  labs(y = 'Number of Publications',
+       x = NULL,
+       title = 'Most Common Fields, 1900-2019',
+       subtitle = 'Source: Web of Science')
 
 # publication title counts
 data %>% 
@@ -75,14 +83,33 @@ data %>%
   geom_col(show.legend = FALSE) +
   coord_flip() +
   theme_minimal() +
-  labs(y = 'Number of Occurrences',
-       x = NULL)
+  labs(y = 'Number of Publications',
+       x = NULL,
+       title = 'Most Common Publication Outlets, 1900-2019',
+       subtitle = 'Source: Web of Science')
 
 # ethics-related records =======================================================
 
 # subset data
 ethics_data <- data %>% 
   filter(str_detect(abstract, "ethic|security|safe|dilemma"))
+
+# count publications per year
+ethics_data %>% 
+  filter(!is.na(pub_year), 
+         pub_year < 2020) %>% 
+  count(pub_year, sort = TRUE) %>% 
+  ggplot(aes(x = pub_year, y = n)) +
+  geom_point() +
+  geom_line() +
+  geom_vline(xintercept = 2007, color = 'orange2') +
+  theme_minimal() +
+  labs(x = 'Publication Year',
+       y = 'Number of Publications',
+       title = 'Number of Ethics-Related Publications Per Year, 1900-2019',
+       subtitle = 'Source: Web of Science',
+       caption = 'Note: Vertical line denotes 2007; years without publications have no point; These publications are a subset of the previous records \n whose abstract contains ethic*, security, safe*, or dilemma..')
+
 
 # top keywords around ethics
 ethics_data %>% 
@@ -95,8 +122,11 @@ ethics_data %>%
   geom_col(show.legend = FALSE) +
   coord_flip() +
   theme_minimal() +
-  labs(y = 'Number of Occurrences',
-       x = NULL) 
+  labs(y = 'Number of Publications',
+       x = NULL,
+       title = 'Top Keywords in Ethics-Related Publications',
+       subtitle = 'Source: Web of Science',
+       caption = 'Note: These publications are a subset of the previous records whose abstract contains ethic*, security, safe*, or dilemma.') 
 
 # count of wos categories
 ethics_data %>% 
@@ -110,4 +140,43 @@ ethics_data %>%
   coord_flip() +
   theme_minimal() +
   labs(y = 'Number of Occurrences',
-       x = NULL)
+       x = NULL,
+       title = 'Most Common Fields in Ethics-Related Publications',
+       subtitle = 'Source: Web of Science',
+       caption = 'Note: These publications are a subset of the previous records whose abstract contains ethic*, security, safe*, or dilemma.')
+
+# publication title counts
+ethics_data %>% 
+  filter(!is.na(pub_title)) %>% 
+  count(pub_title, sort = TRUE) %>% 
+  top_n(20) %>% 
+  ggplot(aes(x = reorder(pub_title, -desc(n)), y = n, fill = pub_title)) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  theme_minimal() +
+  labs(y = 'Number of Publications',
+       x = NULL,
+       title = 'Most Common Outlets in Ethics-Related Publications',
+       subtitle = 'Source: Web of Science',
+       caption = 'Note: These publications are a subset of the previous records whose abstract contains ethic*, security, safe*, or dilemma.')
+
+# distribiution of title counts
+# TODO: figure out how to limit number of titles with same counts
+ethics_data %>% 
+  filter(!is.na(pub_title), !is.na(pub_year)) %>% 
+  mutate(period_indic = case_when(pub_year <= 2007 ~ '<= 2007',
+                                  TRUE ~ '> 2007')) %>% 
+  group_by(period_indic) %>% 
+  count(pub_title, sort = TRUE) %>% 
+  top_n(5, n) %>% 
+  ggplot(aes(x = reorder(pub_title, -desc(n)), y = n, fill = pub_title)) +
+  facet_wrap(~period_indic) +
+  geom_col(show.legend = FALSE) +
+  coord_flip() +
+  theme_minimal() +
+  labs(y = 'Number of Publications',
+       x = NULL,
+       title = 'Most Common Outlets in Ethics-Related Publications',
+       subtitle = 'Source: Web of Science',
+       caption = 'Note: These publications are a subset of the previous records whose abstract contains ethic*, security, safe*, or dilemma.')
+
