2043cfcf1c67504e3b6a8fdda97d382f29f07405
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-01-09 15:17:00 -0700

    problem solving.

--- code/ner_spacy.py
+++ code/ner_spacy.py
@@ -6,9 +6,13 @@
 
 # import modules
 import pandas as pd
-import spacy
 import textacy
 from pathlib import Path
+
+import spacy
+from spacy.lang.char_classes import ALPHA, ALPHA_LOWER, ALPHA_UPPER
+from spacy.lang.char_classes import CONCAT_QUOTES, LIST_ELLIPSES, LIST_ICONS
+from spacy.util import compile_infix_regex
 
 # import data 
 
@@ -27,4 +31,33 @@ abstract_list = [abstract for abstract in df[df['abstract'].str.len() > 0.0]['ab
 # out of the box spacy =========================================================
 
 # create corpus
-abstract_corpus = textacy.Corpus(lang=textacy.load_spacy_lang('eng_core_web_lg'), data=abstract_list)
+abstract_corpus = textacy.Corpus(lang=textacy.load_spacy_lang('en_core_web_lg'), data=abstract_list)
+
+for token in abstract_corpus[0]:
+    print(token.text)
+
+# TODO : add custom tokenization rule 
+
+# default tokenizer
+nlp = spacy.load("en_core_web_lg")
+doc = nlp("mother-in-law")
+
+# modify tokenizer infix patterns
+infixes = (
+    LIST_ELLIPSES
+    + LIST_ICONS
+    + [
+        r"(?<=[0-9])[+\-\*^](?=[0-9-])",
+        r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(
+            al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES
+        ),
+        r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+        # EDIT: commented out regex that splits on hyphens between letters:
+        #r"(?<=[{a}])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
+        r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
+    ]
+)
+
+infix_re = compile_infix_regex(infixes)
+nlp.tokenizer.infix_finditer = infix_re.finditer
+doc = nlp("mother-in-law")
