a68d35eef8ae02db59b9346760f928be0d91335e
Brandon Sepulvado <brandon.sepulvado@gmail.com>
2020-07-09 21:10:40 -0400

    Update example pubmed search.

--- code/example_queries.py
+++ code/example_queries.py
@@ -1,6 +1,16 @@
 from Bio import Entrez
+from pathlib import Path
+import json
+import pandas as pd
 
-Entrez.email = 'sepulvado-brandon@norc.org'
+Entrez.email = 'email'
+
+with open(Path('./code/config.json')) as f:
+    data = json.load(f)
+
+# get api key
+api_key = data['api_key']
+
 # handle = Entrez.esummary(db="pubmed", id="19304878,14630660", retmode="xml")
 # records = Entrez.parse(handle)
 
@@ -9,8 +19,26 @@ Entrez.email = 'sepulvado-brandon@norc.org'
 #     print(record['Title'])
 # handle.close()
 
+test1 = Entrez.esearch(db="pubmed", term="synthetic biology[TIAB]", idtype="acc")
+record = Entrez.read(test1)
+test1.close()
+print(record['Count'])
+
+test2 = Entrez.esearch(db="pubmed", term="'synthetic biology'[TIAB]", idtype="acc")
+record2 = Entrez.read(test2)
+test2.close()
+print(record2['Count'])
+
+# testing
+handle = Entrez.efetch(db="pubmed", id=record2['IdList'], rettype="xml", retmode="text")
+records = Entrez.read(handle)
+handle.close()
+
+get_title_abstract(record['IdList'][0])
+
 # handle = Entrez.esearch(db="pubmed", term="'synthetic biological'[TIAB]", idtype="acc", retmax=10)
 handle = Entrez.esearch(db="pubmed", rettype='count', term="(synthetic biology[TIAB]) AND ethic*[TIAB]", idtype="acc")
+handle = Entrez.esearch(db="pubmed", term="", retmax=10, idtype="acc")
 record = Entrez.read(handle)
 handle.close()
 print(record['Count'])
@@ -19,8 +47,8 @@ print(record['Count'])
 # handle.close()
 
 # write function to get ids; must have previously setup Entrez info
-def get_id_list(search_phrase, max_to_retain=10):
-    handle = Entrez.esearch(db="pubmed", term=search_phrase, idtype="acc", retmax=max_to_retain)
+def get_id_list(search_phrase, max_to_retain=10, api_key = api_key):
+    handle = Entrez.esearch(db="pubmed", term=search_phrase, idtype="acc", retmax=max_to_retain, apikey_var = api_key)
     record = Entrez.read(handle)
     handle.close()
     return record['IdList']
@@ -37,12 +65,47 @@ def get_title_abstract(pubmed_id):
     }
     return dict_title_abstract
 
+# testing
+handle = Entrez.efetch(db="pubmed", id=record['IdList'], rettype="xml", retmode="text")
+records = Entrez.read(handle)
+handle.close()
+
+
+# do this for every author =====================================================
+title = []
+author = []
+pubdate = []
+for num,item in enumerate(records['PubmedArticle']):
+    title.append(item['MedlineCitation']['Article']['ArticleTitle'])
+    # TODO : cleaner function to take non-empty values and extract year
+    pubdate.append(next(iter(item['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate'].values())))
+    name_list = []
+    for name_item in item['MedlineCitation']['Article']['AuthorList']:
+        last_name = name_item['LastName']
+        first_name = name_item['ForeName']
+        name_list.append(last_name + ', ' + first_name)
+    author.append(name_list)
+author_df = pd.DataFrame(
+    list(zip(author, title, pubdate)),
+    columns = ['author', 'title', 'year']
+)
+# end per author operations ====================================================
+    
+    
+name_list = []
+for item in records['PubmedArticle'][0]['MedlineCitation']['Article']['AuthorList']:
+    last_name = item['LastName']
+    first_name = item['ForeName']
+    name_list.append(last_name + ', ' + first_name)
+
+
+
 # # redo for multiple pubmed ids
 # def get_title_abstract(pubmed_ids):
 #     handle = Entrez.efetch(db="pubmed", id=pubmed_ids, rettype="xml", retmode="text")
 
 # test on synthetic biology[TIAB]
-test_result = get_id_list("synthetic biolog*[TIAB]")
+test_result = get_id_list("")
 print(test_result)
 first_id = test_result[0]
 
